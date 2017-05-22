1cf51f252df15e1ba9cb0711977736efe2ba7a2f
Tris Setiawan <tris.setiawan@yahoo.com>
2017-05-22 11:31:02 +0000

    update create spot

--- common/models/spot.js
+++ common/models/spot.js
@@ -2,11 +2,14 @@
 // var loopback = require('loopback');
 // var app = loopback(); 
 module.exports = function(Spot) {
+    const supportedExtensions = ["jpg", "jpeg", "png", "gif"];
+    const regExpExt = /(jpeg|jpg|png|gif)/;
+    const cdnUri = "http://cdn.pojokan.com.global.prod.fastly.net";
 
     Spot.create = function(data, req, res, cb) {
         var Unit = Spot.app.models.Unit;
         var Location = Spot.app.models.Location;
-        // var data = {name:name, description:description, address:address, position:position, phones:phones, category:category, sub:sub, tags:tags};
+
         var unitData = {
             name: data.name,
             description: data.description || "",
@@ -19,12 +22,11 @@ module.exports = function(Spot) {
             address: data.address,
             phones: data.phones || [],
             position: data.position,
-            images: []
+            images: [],
+            tags: data.tags || []
         };
 
         var imageObjects = data.images.map(image => {
-            var supportedExtensions = ["jpg", "jpeg", "png", "gif"];
-            var regExpExt = /(jpeg|jpg|png|gif)/;
             var extension = (image.match(regExpExt) || [null])[0];
             var base64string = image.replace(/^data:image\/[a-z]+;base64,/, "");
             var buffer = new Buffer(base64string, 'base64');
@@ -34,81 +36,68 @@ module.exports = function(Spot) {
                 buffer: buffer
             };
         });
-        var cdnUri = "http://cdn.pojokan.com.global.prod.fastly.net";
-        var extension = imageObjects[0].extension;
-        var buffer = imageObjects[0].buffer;
-        var md5 = require('md5')(buffer);
 
-        var mkdirp = require('mkdirp');
-        var fs = require('fs');
-        var pathUtil = require("path");
-        var getDirName = pathUtil.dirname;
-        var baseDirectory = __dirname;
-        var filename = pathUtil.join(baseDirectory, "../../uploads", `${md5}.${extension}`);
+        var storageOperations = imageObjects.map(imageObject => {
+            return new Promise((resolve, reject) => {
 
-        mkdirp(getDirName(filename), function(err) {
-            if (err) return cb(err);
+                var extension = imageObjects[0].extension;
+                var buffer = imageObjects[0].buffer;
+                var md5 = require('md5')(buffer);
 
-            var wStream = fs.createWriteStream(filename);
-            wStream.on('finish', function() {
-                console.log('file has been written');
-                var keyPath = pathUtil.join(baseDirectory, "../../", 'file/spot-storage-5e984703f061.json');
-                var gcs = require('@google-cloud/storage')({
-                    projectId: 'spot-storage',
-                    keyFilename: keyPath
-                });
-                var bucket = gcs.bucket("regional-bucket-01-us");
-                var options = {
-                    public: true
-                };
+                var mkdirp = require('mkdirp');
+                var fs = require('fs');
+                var pathUtil = require("path");
+                var getDirName = pathUtil.dirname;
+                var baseDirectory = __dirname;
+                var filename = pathUtil.join(baseDirectory, "../../uploads", `${md5}.${extension}`);
 
-                bucket.upload(filename, options, function(err, file, apiResponse) {
-                    // Your bucket now contains:
-                    // - "image.png" (with the contents of `/local/path/image.png')
+                mkdirp(getDirName(filename), function(err) {
+                    if (err) return cb(err);
 
-                    // `file` is an instance of a File object that refers to your new file.
+                    var wStream = fs.createWriteStream(filename);
+                    wStream.on('finish', function() {
+                        console.log('file has been written');
+                        var keyPath = pathUtil.join(baseDirectory, "../../", 'file/spot-storage-5e984703f061.json');
+                        var gcs = require('@google-cloud/storage')({
+                            projectId: 'spot-storage',
+                            keyFilename: keyPath
+                        });
+                        var bucket = gcs.bucket("regional-bucket-01-us");
+                        var options = {
+                            public: true
+                        };
 
-
-                    Unit.create(unitData, (err, unit) => {
-                        if (err)
-                            return cb(err);
-                        locationData.unitId = unit.id;
-                        Location.create(locationData, (err, location) => {
+                        bucket.upload(filename, options, function(err, file, apiResponse) {
                             if (err)
-                                return cb(err);
-                            data.id = unit.id;
-                            cb(null, data);
+                                reject(err);
+                            else
+                                resolve(file);
                         });
                     });
-
+                    wStream.write(buffer);
+                    wStream.end();
                 });
             });
-            wStream.write(buffer);
-            wStream.end();
         });
-        // var gcs = require('@google-cloud/storage')({
-        //     projectId: 'spot-storage',
-        //     keyFilename: '../file/spot-storage-5e984703f061.json'
-        // });
-        // var bucket = gcs.bucket("regional-bucket-01-us");
-        // var options = {
-        //     entity: 'allUsers',
-        //     role: gcs.acl.READER_ROLE
-        // };
-        // bucket.acl.add(options, function(err, aclObject) {});
-        // console.log(imageBinaries);
 
-        // Unit.create(unitData, (err, unit) => {
-        //     if (err)
-        //         return cb(err);
-        //     locationData.unitId = unit.id;
-        //     Location.create(locationData, (err, location) => {
-        //         if (err)
-        //             return cb(err);
-        //         data.id = unit.id;
-        //         cb(null, data);
-        //     });
-        // });
+        Promise.all(storageOperations)
+            .then(files => {
+                Unit.create(unitData, (err, unit) => {
+                    if (err)
+                        return cb(err);
+                    locationData.unitId = unit.id;
+                    locationData.images = files.map(file => file.name);
+                    Location.create(locationData, (err, location) => {
+                        if (err)
+                            return cb(err);
+                        data.id = unit.id;
+                        cb(null, data);
+                    });
+                });
+            })
+            .catch(ex => {
+                cb(ex);
+            });
     };
 
     Spot.remoteMethod("create", {
